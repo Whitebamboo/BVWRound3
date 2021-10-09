28300ff7852ed4505e8be7428a5b520ffa7a35f6
2015301140020@whu.edu.cn <Luna0minos4>
2021-10-08 21:43:06 -0400

    Upload gameflow in the JenniferTest.unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+public enum GameState
+{
+    Start,
+    Intro,
+    Guest,
+    Ending
+}
+
+public enum GuestState
+{
+    Start,
+    Dialog,
+    WaitForGesture,
+    Response,
+    End
+}
+
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +27,19 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance => s_Instance;
 
     public PersonData[] data;
+    public GameObject TextTyper;
+    public GestureEnum gestureState; 
+    public int Reputation = 0;
+
+
+    [HideInInspector] public GameState state;
+    [HideInInspector] public GuestState guestState;
+
+    private bool GestureFinished = false;
+    //person index
+    private int personIndex = 0;
+    //dialog index
+    private int dialogIndex = 0;
 
     void Awake()
     {
@@ -20,5 +52,160 @@ public class GameManager : MonoBehaviour
         s_Instance = this;
     }
 
+    //Start ----
+    private void Start()
+    {
+        TextTyper = GameObject.FindGameObjectWithTag("Typer");
 
+        StartCoroutine("TestCode");
+    }
+    private void Update()
+    {
+        print(state);
+        print(guestState);
+        if (state == GameState.Start)
+        {
+            state = GameState.Intro;
+        }
+        if (state == GameState.Intro)
+        {
+            state = GameState.Guest;
+        }
+
+        if (state == GameState.Guest)
+        {
+            if (guestState == GuestState.Start)
+            {
+                EachGuestStart();
+            }
+            else if(guestState == GuestState.Dialog)
+            {
+                
+            }
+            else if(guestState == GuestState.WaitForGesture)
+            {
+                //
+                if (GestureFinished)
+                {
+                    guestState = GuestState.Response;
+                    AskTextTyperShowResponse(data[personIndex]);
+                }
+            }
+            else if (guestState == GuestState.Response)
+            {
+                
+            }
+            else if (guestState == GuestState.End)
+            {
+                guestLeaving();
+            }
+        }
+
+        //Test, instead of gesture
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            gestureState = GestureEnum.ThumbsUp;
+            GestureFinished = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            gestureState = GestureEnum.ThumbsDown;
+            GestureFinished = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            gestureState = GestureEnum.MiddleFinger;
+            GestureFinished = true;
+        }
+    }
+    
+    
+    void EachGuestStart()
+    {
+        //clear
+        guestState = GuestState.Dialog;
+        CreateCharacterImageAndMove();
+        AskTextTyperShowDialogData(data[personIndex]);
+    }
+
+    //1 Someone comes
+    void CreateCharacterImageAndMove()
+    {
+        //TODO
+    }
+    //2 Ask Text Typer to show
+    void AskTextTyperShowDialogData(PersonData personData)
+    {
+        DialogPack dp = personData.DialogData[dialogIndex];
+        //ugly
+        object[] message = new object[2];
+        message[0] = dp.Dialogs;
+        message[1] = personData.PeopleName;
+
+        TextTyper.SendMessage("ReceiveDialogEvent", message, SendMessageOptions.RequireReceiver);
+    }
+
+    //3 Wait for gesture
+    public void WhenTypingCompletedEvent()
+    {
+        if (guestState == GuestState.Dialog)
+        {
+            guestState = GuestState.WaitForGesture;
+        }
+        else if (guestState == GuestState.Response)
+        {
+            //if other dialog
+            if (dialogIndex < data[personIndex].DialogData.Length - 1)
+            {
+                dialogIndex++;
+                EachGuestStart();
+            }
+            else
+            {
+                guestState = GuestState.End;
+            }
+
+            
+        }
+        //Dialog or response Completed
+        
+        //TO DO
+        print("WhenDialogCompletedEvent");
+    }
+
+
+    void AskTextTyperShowResponse(PersonData personData)
+    {
+        print("AskTextTyperShowResponse");
+        string[] stringArr = personData.DialogData[dialogIndex].Responses[(int)gestureState].Response;
+
+        object[] message = new object[2];
+        message[0] = stringArr;
+        message[1] = personData.PeopleName;
+
+        TextTyper.SendMessage("ReceiveDialogEvent", message, SendMessageOptions.RequireReceiver);
+
+    }
+    //Guest leaves
+    void guestLeaving()
+    {
+        //TO DO
+        GestureFinished = false;
+        if (personIndex < data.Length - 1)
+        {
+            personIndex++;
+            guestState = GuestState.Start;
+        }
+        else
+        {
+            //Enter ending
+            state = GameState.Ending;
+        }
+    }
+
+    IEnumerator TestCode()
+    {
+        yield return new WaitForSeconds(5);
+        //GestureFinished = true;
+    }
 }
