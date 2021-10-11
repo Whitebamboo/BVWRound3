4d1e7dd45b00f30136a0996e5e0d55ca9aaac5b4
Jennifer03333333332 <yingxual@andrew.cmu.edu>
2021-10-11 18:58:14 -0400

    Update EndSceneUI.cs
    End Scene UI

--- Assets/Scripts/UI/EndSceneUI.cs
+++ Assets/Scripts/UI/EndSceneUI.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class EndSceneUI : MonoBehaviour
 {
+    
+    public static EndSceneUI instance;
 
+    private static GameObject ScoreTxt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        instance = this;
+        ScoreTxt = transform.Find("Score").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public static void ShowEndSceneUI()
+    {
+        ScoreTxt.GetComponent<TMPro.TextMeshProUGUI>().text = GameManager.Reputation.ToString();
+        instance.gameObject.SetActive(true);
+        //+ " / 100";
     }
 }
